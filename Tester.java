0bc4960a47500fdb2c195962379db0174e80e8a9
Yuqing Wu <ywu20@stuy.edu>
2020-10-27 16:40:19 -0400

    deposit test

--- Tester.java
+++ Tester.java
@@ -1,10 +1,21 @@
 public class Tester{
   public static void main(String[] args){
+    //constructor + get methods test
     BankAccount a = new BankAccount(0,"1234567");
     System.out.println(a.getBalance());
     System.out.println(a.getAccountID());
-    a.setPassword("987654");
+
+    //set method test
+    a.setPassword("987qeE/s4");
     System.out.println(a.getPassword());
+
+    //deposit method test
+    System.out.println(a.deposit(23.8));
+    System.out.println(a.getBalance());
+    System.out.println(a.deposit(-2.2));
+    System.out.println(a.getBalance());
+    System.out.println(a.deposit(0));
+    System.out.println(a.getBalance());
 
   }
 }
