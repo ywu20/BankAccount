cc950e0865e08c46a38ea76846d5a4033b1143a6
Yuqing Wu <ywu20@stuy.edu>
2020-10-27 16:31:56 -0400

    constructor & get methods test

---
+++ Tester.java
@@ -0,0 +1,7 @@
+public class Tester{
+  public static void main(String[] args){
+    BankAccount a = new BankAccount(0,"1234567");
+    System.out.println(a.getBalance());
+    System.out.println(a.getAccountID());
+  }
+}
